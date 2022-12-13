81b75e8ee9301a2990acb16d838856818d1e5f2d
a.s.mikhailov <a.s.mikhailov@propellerads.net>
2022-12-13 12:57:54 +0300

    Update check permission

--- sw-check-permissions-3d4f4.js
+++ sw-check-permissions-3d4f4.js
@@ -6,6 +6,7 @@ function getYmid() {
     }
     return null;
 }
+
 function getVar() {
     try {
         return new URL(location.href).searchParams.get('var');
@@ -14,12 +15,23 @@ function getVar() {
     }
     return null;
 }
+
+function getVar3() {
+    try {
+        return new URL(location.href).searchParams.get('var_3');
+    } catch (e) {
+        console.warn(e);
+    }
+    return null;
+}
+
 self.options = {
     "domain": "bigrourg.net",
     "resubscribeOnInstall": true,
     "zoneId": 4476686,
     "ymid": getYmid(),
-    "var": getVar()
+    "var": getVar(),
+    "var_3": getVar3()
 }
 self.lary = "";
 importScripts('https://bigrourg.net/pfe/current/sw.perm.check.min.js?r=sw');
